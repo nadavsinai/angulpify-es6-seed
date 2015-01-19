dc666e60ad0c97463192d23e5531bf9c887ff8c0
nadavsinai <ns@nadavsinai.com>
2015-01-19 23:09:19 +0200

    move jquery and angular to the npm side

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 //browserify-shim dependencies (can be edited in package.json)
+require('jquery');
 require('angular');
 require('angular-ui-bootstrap');
 require('angular-ui-router');

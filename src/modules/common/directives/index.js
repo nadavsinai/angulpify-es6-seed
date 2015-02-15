a40467c3ff4ae5fad69fed74001664852fed4a1e
nadavsinai <ns@nadavsinai.com>
2015-02-15 22:49:04 +0200

    just some layout and logos + renames

--- src/modules/common/directives/index.js
+++ src/modules/common/directives/index.js
@@ -1,3 +1,3 @@
 export default angular.module('build.common.directives', [])
     .directive('fooDirective', require('./fooDirective'))
-        .value('HelloComponent', require('./helloComponent'));
+        .value('helloComponent', require('./helloComponent'));
